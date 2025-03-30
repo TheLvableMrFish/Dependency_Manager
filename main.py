94eac77bdcdc2f001584c15477f106fc939316ba
TheLvableMrFish <paganochristians@gmail.com>
2025-03-29 20:45:59 -0500

    added function and the start of tkinter

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-path = ""
-fileName = ""
-groups = [[],[]]
+import tkinter as tk
+from tkinter import ttk
+import csv
+import os
+
+def create_dependency(name, path, data):
+    try:
+        # Make sure path exists
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        # Make sure file doesn't exist
+        if os.path.exists(path):
+            return
+        
+        with open(path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(data)
+        print(f"CSV file created as : {path} for {name}")
+    
+    except Exception as e:
+        print(f"Error creating {name} CSV file: {e}")
+
+path = "temp"
+
+# window
+window = tk.Tk()
+window.title('Dependency Manager')
+window.geometry('600x400')
+
+# run
+window.mainloop()
