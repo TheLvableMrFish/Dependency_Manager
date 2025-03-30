8763cf74b26744482684a5c4343f67fe5ae6fe0c
TheLvableMrFish <paganochristians@gmail.com>
2025-03-29 21:18:23 -0500

    added a start to nav menu

--- main.py
+++ main.py
@@ -33,16 +33,41 @@ def generate_csv():
 window = tk.Tk()
 window.title('Dependency Manager')
 window.geometry('600x400')
+window.minsize(295,100)
+
+notebook = ttk.Notebook(window)
+tab1 = ttk.Frame(notebook)
+# tab2 = ttk.Frame(notebook)
+# tab3 = ttk.Frame(notebook)
+
+# Tab 1
+notebook.add(tab1, text = 'Tab 1')
+# label_tab1 = ttk.Label(tab1, text='Text in tab 1')
+# label_tab1.pack(side='top', anchor='w')
+# button_tab1 = ttk.Button(tab1, text="Button in tab 1")
+# button_tab1.pack(side='top', anchor='w')
 
 # Name Entry
-name_label = ttk.Label(window, text="Name:")
+# name_label = ttk.Label(tab1, text="Name:")
+# name_label.pack(padx='5', pady='5', side='left', anchor='w')
+# name_entry = ttk.Entry(tab1)
+# name_entry.pack(padx='5', pady='5', side='left', anchor='w')
+
+# # Generate Dependency Button
+# dependency_btn = ttk.Button(tab1, text="Create", command=generate_csv)
+# dependency_btn.pack(padx='5', pady='5', side='left', anchor='w')
+
+# Name Entry
+name_label = ttk.Label(tab1, text="Name:")
 name_label.grid(row=0, column=0, padx=10, pady=5, sticky='w')
-name_entry = ttk.Entry(window)
+name_entry = ttk.Entry(tab1)
 name_entry.grid(row=0, column=1, padx=10, pady=5, sticky='w')
 
 # Generate Dependency Button
-dependency_btn = ttk.Button(window, text="Create", command=generate_csv)
-dependency_btn.grid(row=0, column=2, columnspan=2, pady=5)
+dependency_btn = ttk.Button(tab1, text="Create", command=generate_csv)
+dependency_btn.grid(row=0, column=2, padx=10, pady=5, sticky='w')
+
+notebook.pack(fill='both')
 
 # run
 window.mainloop()
